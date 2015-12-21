b49a806e64860060bd487207db32a64f4c6fc0c2
Angel Ramboi <angel@ramboi.com>
2015-12-21 17:24:07 +0100

    Added piwik url and id for analytics

--- publishconf.py
+++ publishconf.py
@@ -13,6 +13,9 @@ RELATIVE_URLS = False
 DISQUS_SITENAME = 'altshiftkill'
 DISQUS_ON_PAGES = False
 
+PIWIK_URL = 'piwik.altshiftkill.com'
+PIWIK_SITE_ID = 1
+
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
