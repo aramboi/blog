d777144258603a17c1f901bc98cb46280e699820
Angel Ramboi <angel@ramboi.com>
2020-02-09 15:12:29 +0100

    Move output to docs folder

--- publishconf.py
+++ publishconf.py
@@ -9,13 +9,8 @@ SITEURL = 'https://www.angelramboi.com'
 FEED_DOMAIN = SITEURL
 RELATIVE_URLS = False
 
-# DISQUS_SITENAME = 'altshiftkill'
-# DISQUS_ON_PAGES = False
-
-# PIWIK_URL = 'altshiftkill.com/pwk'
-# PIWIK_SITE_ID = 1
-
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
+OUTPUT_PATH = 'docs/'
 DELETE_OUTPUT_DIRECTORY = True
