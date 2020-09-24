1f8bc8d7a59464a111e6fbab63a42110e565d75e
Angel Ramboi <angel@ramboi.com>
2020-09-24 19:06:31 +0200

    Add analytics tag

--- publishconf.py
+++ publishconf.py
@@ -15,3 +15,5 @@ UTTERANCES_REPO = 'aramboi/blog'
 UTTERANCES_MAPPING = 'title'
 UTTERANCES_LABEL = 'comments'
 UTTERANCES_THEME = 'github-light'
+
+ACKEE_TAG = '<script async src="https://a.angelramboi.com/a.js" data-ackee-server="https://a.angelramboi.com" data-ackee-domain-id="57e37041-c751-4008-b25e-fc87059487b7"></script>'
