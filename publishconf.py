f9af813551dbafdf30a672c6faa4e4da6283f8ac
Angel Ramboi <angel@ramboi.com>
2020-12-10 09:49:49 +0200

    Update analytics script

--- publishconf.py
+++ publishconf.py
@@ -16,4 +16,4 @@ UTTERANCES_MAPPING = 'title'
 UTTERANCES_LABEL = 'comments'
 UTTERANCES_THEME = 'github-light'
 
-ACKEE_TAG = '<script async src="https://a.angelramboi.com/a.js" data-ackee-server="https://a.angelramboi.com" data-ackee-domain-id="57e37041-c751-4008-b25e-fc87059487b7"></script>'
+ANALYTICS_TAGS = """<!-- Cloudflare Web Analytics --><script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "6badc6d8d8874100b4e1dde83a4f5f51"}'></script><!-- End Cloudflare Web Analytics -->"""
